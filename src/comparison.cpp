e7ba2878c5926fc1d3df35caac22d22cf9fd44e6
skfro6360 <skfro6360@snu.ac.kr>
2017-07-24 18:32:47 +0900

    Update comparison.cpp

--- src/comparison.cpp
+++ src/comparison.cpp
@@ -1,9 +1,8 @@
 #include "comparison.h"
 
-printf("Hello world!");
 // Comparison Test over the Integer
 // if lessThan = 1, lessThan circuit
 // if lessThan = 0, greaterThan circuit
 void comparisonTest(){
-    
+    printf("Hello world!");
 }
