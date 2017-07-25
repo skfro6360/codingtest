3ded3a7099ea3b5944eb7dff26d7fc72d22b1a42
skfro6360 <skfro6360@snu.ac.kr>
2017-07-25 17:46:51 +0900

    Update comparison.cpp

--- src/comparison.cpp
+++ src/comparison.cpp
@@ -1,5 +1,14 @@
 #include "comparison.h"
 
+void generateMsg(){
+  vector<ZZX> v;
+  v.resize(nslots);
+  for (long i = 0; i < nslots; i++) {
+    GF2X f;
+    random(f, n);
+    conv(v[i], f);
+  } 
+}
 // Comparison Test over the Integer
 // if lessThan = 1, lessThan circuit
 // if lessThan = 0, greaterThan circuit
